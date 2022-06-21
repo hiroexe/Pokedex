9761d3124a24c32c4c3312bb15c136f4206643c8
i.bettini <i.bettini@gmail.com>
2022-06-21 15:04:41 +0200

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Ivo Junior Bettini on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
