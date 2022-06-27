5d239d8b0d5f78f9b64ae5e540cc7e7381fb84ce
i.bettini <i.bettini@gmail.com>
2022-06-27 15:49:42 +0200

    update

--- pokedex/View/ContentView.swift
+++ pokedex/View/ContentView.swift
@@ -137,36 +137,3 @@ class SizeParserMini: ObservableObject{
     }
 }
 
-/*
-func playSound(name: String) {
-        
-    var player : AVAudioPlayer?
-        //print("name")
-        
-    guard let url = Bundle.main.url(forResource: "mp3/\(name)", withExtension: "mp3") else {
-            print("RETURNING ")
-            print("\(name)")
-            return
-        }
-        
-        
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-            
-            /* iOS 10 and earlier require the following line:
-             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-            
-            guard let player = player else { return }
-            
-            player.play()
-            
-        } catch let error {
-            print("Error was detected")
-            print(error.localizedDescription)
-        }
-    }
-*/
